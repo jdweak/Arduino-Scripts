16291335e9d9838a5a0ed7abf39c73f591427ff7
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2022-02-10 14:06:17 -0800

    Create color-wheel.py

---
+++ color-wheel.py
@@ -0,0 +1,15 @@
+#spins lights in a circle
+counter = 0
+prev = 0
+cp.red_led = True
+
+while True:
+    cp.red_led = not cp.red_led
+    if counter == 0:
+        prev = 9
+    else:
+        prev = counter - 1
+    cp.pixels[counter] = (50, 50, 0)
+    cp.pixels[prev] = (0, 0, 0)
+    counter = (counter + 1) % 10
+    time.sleep(0.5)
