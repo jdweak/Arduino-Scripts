5129dd5d34162fe2ae3986da69ec2ef4e197ab2c
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2022-02-23 22:37:21 -0800

    switch to counter implementation

--- snack-box.py
+++ snack-box.py
@@ -6,28 +6,33 @@ from adafruit_circuitplayground import cp
 from adafruit_hcsr04 import HCSR04
 from time import sleep
 from Queue import Queue
+import adafruit_motor.servo
+import pwmio
 
 
 # Setup Section
 led_brightness = 0.25
 t = 0
-dt = 0.25
+dt = 0.1
 sonar = HCSR04(trigger_pin=board.TX, echo_pin=board.A6)
 cp.pixels.fill((255,0,0))
 cp.pixels.brightness = 0
+pwm = pwmio.PWMOut(board.A1, frequency=50)
+servo = adafruit_motor.servo.Servo(pwm, min_pulse=750, max_pulse=2600)
 
-#how many past samples to consider in the running average
-sampleRangeSize = 100
-#queue to store most recent distance data points
-distanceData = Queue(sampleRangeSize)
-#measured distance between closed door and sensor
-doorDistance = 10
-#sum of distance points in the distanceData queue
-totalDistance = doorDistance
-#average of distance points in the distanceData queue
-averageDistance = doorDistance
+#how many samples must be over the limit for it to be open
+numOpenSampleThreshold = 10
 #theshold for considering the door open based on averageDistance
 openDistanceThreshold = 12
+#is the door open
+doorOpen = False
+#counter of how many times the door has been open
+doorOpenCounter = 0
+#amount of snacks per day
+snackLimit = 3
+#amount of snacks left in current day
+snacksLeft = snackLimit
+
 
 # Function Section
 def pixel_flip():
@@ -36,28 +41,39 @@ def pixel_flip():
     else:
         cp.pixels.brightness = led_brightness
             
+            
+def openDoor():
+    servo.angle = 0
 
-def initializeQueue():
-    for i in range(sampleRangeSize):
-        distanceData.enqueue(doorDistance)
-        
+def closeDoor():
+    servo.angle = 180
 
-#Initialize section
-initializeQueue()
+openDoor()
 
 # Loop Section
 while True:
     try:
         #take the current distance point from door
         newDistance = sonar.distance
-        #update the average distance and distance data queue
-        totalDistance = totalDistance - int(distanceData.dequeue())
-        totalDistance = totalDistance + newDistance
-        distanceData.enqueue(newDistance)
-        averageDistance = totalDistance / distanceData.qsize()
         
-        print('average distance: ', averageDistance)
-        
+
+        if newDistance > openDistanceThreshold:
+            doorOpenCounter += 1
+            if doorOpenCounter == numOpenSampleThreshold:
+                doorOpen = True
+                cp.play_file("one-punch.wav")
+
+        else:
+            doorOpenCounter = 0
+            if doorOpen == True:
+                doorOpen = False
+                snacksLeft -= 1
+            if(snacksLeft == 0):
+                closeDoor()
+    
+        print('door open counter: ', doorOpenCounter)
+        print('door open t/f: ', doorOpen)
+        print('snacks left: ', snacksLeft)
         
     except RuntimeError:
         print("Retrying!")
