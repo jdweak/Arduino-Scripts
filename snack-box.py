2be39b8edc30be4f28ac463b2bd0075ed54ba60e
Jonathan Weakliem <67931921+jdweak@users.noreply.github.com>
2022-02-25 17:48:32 -0800

    Prototype 1
    Added number lights, reseting after interval, door opening and closeing on intervals

--- snack-box.py
+++ snack-box.py
@@ -8,17 +8,33 @@ from time import sleep
 from Queue import Queue
 import adafruit_motor.servo
 import pwmio
+import digitalio
 
 
 # Setup Section
 led_brightness = 0.25
-t = 0
+t = 0.0
 dt = 0.1
+sleepTime = 0.0
 sonar = HCSR04(trigger_pin=board.TX, echo_pin=board.A6)
 cp.pixels.fill((255,0,0))
 cp.pixels.brightness = 0
 pwm = pwmio.PWMOut(board.A1, frequency=50)
 servo = adafruit_motor.servo.Servo(pwm, min_pulse=750, max_pulse=2600)
+
+#led lights setup. the numbers after the led represents the light that should be on when that many snacks are left
+ledDoor = digitalio.DigitalInOut(board.A2)
+ledDoor.direction = digitalio.Direction.OUTPUT
+ledDoor.value = False
+led3 = digitalio.DigitalInOut(board.A3)
+led3.direction = digitalio.Direction.OUTPUT
+led3.value = True
+led2 = digitalio.DigitalInOut(board.A4)
+led2.direction = digitalio.Direction.OUTPUT
+led2.value = True
+led1 = digitalio.DigitalInOut(board.A5)
+led1.direction = digitalio.Direction.OUTPUT
+led1.value = True
 
 #how many samples must be over the limit for it to be open
 numOpenSampleThreshold = 10
@@ -32,7 +48,8 @@ doorOpenCounter = 0
 snackLimit = 3
 #amount of snacks left in current day
 snacksLeft = snackLimit
-
+#How long in seconds there is between a reset of snacksLeft (default 24 hours)
+resetInterval = 20
 
 # Function Section
 def pixel_flip():
@@ -48,33 +65,60 @@ def openDoor():
 def closeDoor():
     servo.angle = 180
 
-openDoor()
+#reset all values to full after a new interval has passed
+def resetHardware():
+    openDoor()
+    led3.value = True
+    led2.value = True
+    led1.value = True
 
 # Loop Section
 while True:
     try:
+        print('mod value: ', t % resetInterval) 
+        #when the time is on the reset time, reset all variables to beginning of day values
+        if round((t % resetInterval), 1) == 0.1:
+            resetHardware()
+            snacksLeft = snackLimit
+            print('reseting')
+        
         #take the current distance point from door
         newDistance = sonar.distance
         
-
+        #if the door is determined to be open
         if newDistance > openDistanceThreshold:
             doorOpenCounter += 1
             if doorOpenCounter == numOpenSampleThreshold:
                 doorOpen = True
-                cp.play_file("one-punch.wav")
-
+            if doorOpenCounter > numOpenSampleThreshold * 3 and doorOpenCounter % 20 == 0:
+                t += .3
+                cp.play_tone(800, 0.3)
+                ledDoor.value = not ledDoor.value
+        #the door is closed
         else:
             doorOpenCounter = 0
+            ledDoor.value = False
             if doorOpen == True:
                 doorOpen = False
                 snacksLeft -= 1
-            if(snacksLeft == 0):
+            if snacksLeft == 2:
+                led3.value = False
+            elif snacksLeft == 1:
+                led2.value = False
+            elif snacksLeft == 0:
+                led1.value = False
                 closeDoor()
+                sleepTime = resetInterval - (t % resetInterval)
+                t += sleepTime
+                sleep(sleepTime)
     
         print('door open counter: ', doorOpenCounter)
         print('door open t/f: ', doorOpen)
         print('snacks left: ', snacksLeft)
+        print('time: ', t)
         
     except RuntimeError:
         print("Retrying!")
+    t += dt
+    t = round(t, 1)
     sleep(dt)
